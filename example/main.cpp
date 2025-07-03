d0da0459b0485b4f7f31eec70f04b7e6673ac4a9
Patrick Schuur <patrickschuurdev@gmail.com>
2025-07-03 21:16:21 +0200

    SDL setup

--- example/main.cpp
+++ example/main.cpp
@@ -1,7 +1,36 @@
 ﻿#include <iostream>
 #include <graphics_utility.hpp>
+#include <SDL3/SDL.h>
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	SDL_Window* window;
+	bool done = false;
+
+	SDL_Init(SDL_INIT_VIDEO);
+
+	window = SDL_CreateWindow(
+		"An SDL3 window",
+		640,
+		480,
+		SDL_WINDOW_OPENGL
+	);
+
+	if (window == nullptr) {
+		SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
+		return 1;
+	}
+
+	while (!done) {
+		SDL_Event event;
+
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_EVENT_QUIT) {
+				done = true;
+			}
+		}
+	}
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 	return 0;
 }
